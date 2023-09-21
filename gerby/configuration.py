3f63ecf086e68b95d3baee1cfb45d4fe61536530
Emily <algebraic.curve@gmail.com>
2023-09-21 17:10:36 -0300

    >

---
+++ gerby/configuration.py
@@ -0,0 +1,11 @@
+# configuration for the website
+COMMENTS = "comments.sqlite"
+DATABASE = "stacks.sqlite"
+UNIT = "section"
+DEPTH = 0
+
+# configuration for the import
+PATH = "stacks"
+PAUX = "stacks.paux"
+TAGS = "stacks.tags"
+PDF = "stacks.pdf"
